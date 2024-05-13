338a238b3f7f6c7be77d99b82d15e1b656cc9d2c
leanaisemberg <leanaisemberg@gmail.com>
2024-05-13 19:29:44 -0300

    Tercera entrega

--- js/main.js
+++ js/main.js
@@ -1,75 +1,205 @@
 // Informacion de los Pokemones iniciales
 const pokemonInfo = [
-    {id: 1, nombre: 'Bulbasaur', tipo: 'planta', generacion: 1 },
-    {id: 2, nombre: 'Charmander', tipo: 'fuego', generacion: 1 },
-    {id: 3, nombre: 'Squirtle', tipo: 'agua', generacion: 1 },
-    {id: 4, nombre: 'Chikorita', tipo: 'planta', generacion: 2 },
-    {id: 5, nombre: 'Cyndaquil', tipo: 'fuego', generacion: 2 },
-    {id: 6, nombre: 'Totodile', tipo: 'agua', generacion: 2 },
-    {id: 7, nombre: 'Treecko', tipo: 'planta', generacion: 3 },
-    {id: 8, nombre: 'Torchic', tipo: 'fuego', generacion: 3 },
-    {id: 9, nombre: 'Mudkip', tipo: 'agua', generacion: 3 },
+    {id: 1, nombre: 'Bulbasaur', tipo: 'planta', sprite:'./img/1.png', generacion: 1 },
+    {id: 2, nombre: 'Charmander', tipo: 'fuego', sprite:'./img/2.png', generacion: 1 },
+    {id: 3, nombre: 'Squirtle', tipo: 'agua', sprite:'./img/3.png', generacion: 1 },
+    {id: 4, nombre: 'Chikorita', tipo: 'planta', sprite:'./img/4.png', generacion: 2 },
+    {id: 5, nombre: 'Cyndaquil', tipo: 'fuego', sprite:'./img/5.png', generacion: 2 },
+    {id: 6, nombre: 'Totodile', tipo: 'agua', sprite:'./img/6.png', generacion: 2 },
+    {id: 7, nombre: 'Treecko', tipo: 'planta', sprite:'./img/7.png', generacion: 3 },
+    {id: 8, nombre: 'Torchic', tipo: 'fuego', sprite:'./img/8.png', generacion: 3 },
+    {id: 9, nombre: 'Mudkip', tipo: 'agua', sprite:'./img/9.png', generacion: 3 },
 ]
 
-// Funcion mostrar lista de pokemones
-function obtenerPokemonLista (array) { 
-    let lista = array.map ((elemento) => elemento.id + '. ' + elemento.nombre).join('\n') 
-    alert("Esta es la lista de pokemones iniciales disponibles:\n" + lista)
-}
+// Evento principal
+document.addEventListener('DOMContentLoaded', function() {
+    const catchProbability = 0.35; // Probabilidad de atrapar
+  
+    const team = JSON.parse(localStorage.getItem('team')) || []; // Obtener el equipo del localStorage
+    const teamLimit = 6; // Número máximo de Pokémon en el equipo
+  
+    const pokemonIdInput = document.getElementById('pokemonId');
+    const messageText = document.querySelector('.message');
+    const teamList = document.getElementById('teamList');
+    const confirmationModal = document.getElementById('confirmationModal');
+    const confirmCatchButton = document.getElementById('confirmCatchButton');
+    const cancelCatchButton = document.getElementById('cancelCatchButton');
+    const resetButton = document.getElementById('resetButton');
 
-// Funcion para obtener pokemon elegido
-function obtenerPokemonNombre(array, objeto1, objeto2) {
-    let nombreParcial = array.filter((elemento) => {
-        return elemento.tipo === objeto1 && elemento.generacion === objeto2
-    }) 
-    
-    // En caso que no ingrese nada o algo incorrecto, se desbloquea un Easter Egg
-    if (nombreParcial.length === 0) {
-        nombreParcial = 'Pikachu'
-        alert('No has seleccionado ningún pokemon, así que te hemos asignado al pokemon secreto ¡Pikachu!')
-        return nombreParcial
-    } else {
-        return nombreParcial[0].nombre 
-    }   
-}
+    // Evento click "Reiniciar equipo"
+    resetButton.addEventListener('click', function() {
+      resetTeamAndPokeballs();
+    });
 
-// Funcion para saludar al entrenador y que ingrese sus datos
-function saludoEntrenador(array) {
-    let genero = Number(prompt('¿Eres un chico, una chica o no te identificas con ninguno?\n1 Chico\n2 Chica\n3 No me siento identificado con ninguno'));
-    let nombreEntrenador = prompt('¿Cuál es tu nombre?');
-    let comida = prompt('¿Cuál es tu comida favorita?');
+    const pokeballsLeftText = document.querySelector('.pokeballs-left');
 
-    obtenerPokemonLista (array)
+    let tries = 20; // Número máximo de intentos (Pokeballs)
+    const pokeballPrice = 10; // Precio de cada Pokéball
+    let pokeballsLeft = parseInt(localStorage.getItem('pokeballsLeft')) || tries; // Obtener las Pokébolas restantes del localStorage
 
-    let pokemonTipo = prompt('Elegí tu tipo favorito. Los disponibles son:\n- Fuego\n- Agua\n- Planta').toLowerCase()
-    let pokemonGen = Number(prompt('Elegí tu generación favorito (ingresa solo el número). Las disponibles son:\nGeneración 1\nGeneración 2\nGeneración 3'))
+    const catchButton = document.getElementById('catchButton');
 
-    let pokemonNombre = obtenerPokemonNombre (array, pokemonTipo, pokemonGen)
-
-    // Condicionales Easter Egg
-    if (nombreEntrenador.length === 0) {
-        nombreEntrenador = 'Ash'
-    }
+    // Evento click "Lanzá una pokeball"
+    catchButton.addEventListener('click', function() {
+        const pokemonId = parseInt(pokemonIdInput.value);
         
-    if (comida.length === 0) {
-        comida = 'Paleta'
-    }
+        let pokemonElegido = getPokemonInfo (pokemonInfo, pokemonId) // Guarda pokemon elegido
+
+        if (isNaN(pokemonId)) {
+            showMessage('ID de Pokémon no válido. Por favor, introduce un valor numérico.');
+            return;
+        }
+
+        if (pokeballsLeft > 0) {
+            showConfirmationModal(pokemonElegido.name);
+          } else {
+            showMessage(`Te has quedado sin Pokéballs. Visita la tienda más cercana para conseguir más por ${pokeballPrice} creditos cada una.`);
+          }
+    });
+
+    // Evento click "Si"
+    confirmCatchButton.addEventListener('click', function() {
+        confirmationModal.style.display = 'none';
+        const pokemonId = parseInt(pokemonIdInput.value);
+        let pokemonElegido = getPokemonInfo(pokemonInfo, pokemonId)
+        tryToCatchPokemon(pokemonElegido.id, pokemonElegido.name, pokemonElegido.type, pokemonElegido.sprite);
+    });
     
-    // Condicional donde se conjugan todos los datos para seleccionar el saludo al entrenador y se le informa de su pokemon elegido
-    if (genero === 1) {
-      alert('¡Hola, entrenador ' + nombreEntrenador + ' de pueblo ' + comida + '!' + ' Has elegido a ' + pokemonNombre + '.');
-    } else if (genero === 2) {
-      alert('¡Hola, entrenadora ' + nombreEntrenador + ' de pueblo ' + comida + '!' + ' Has elegido a ' + pokemonNombre + '.');
-    } else {
-      alert('Te damos la bienvenida, ' + nombreEntrenador + ' de pueblo ' + comida + '!' + ' Has elegido a ' + pokemonNombre + '.');
+    // Evento click "No"
+    cancelCatchButton.addEventListener('click', function() {
+        confirmationModal.style.display = 'none';
+        showMessage('Decidiste no atrapar al Pokémon.');
+    });
+
+    // Funcion mostrar modal
+    function showConfirmationModal(pokemonName) {
+        const modalMessage = document.querySelector('.modal-message');
+        modalMessage.textContent = `¿Estás seguro de que quieres atrapar a ${pokemonName}?`;
+        confirmationModal.style.display = 'block';
     }
-}
 
-saludoEntrenador(pokemonInfo);
+    // Funcion reiniciar todo
+    function resetTeamAndPokeballs() {
+        team.length = 0; // Vaciar el equipo
+        pokeballsLeft = tries; // Restablecer las Pokébolas restantes
+        
+        saveTeamAndPokeballs(); // Guardar los cambios en el localStorage
+      
+        showMessage('El equipo y los intentos han sido reseteados.'); // Mostrar un mensaje de confirmación
+      
+        displayTeamMembers(); // Actualizar la visualización del equipo
+        updatePokeballsLeftText(); // Actualizar el texto de las Pokébolas restantes
+    }
 
+    // Funcion simulador atrapador pokemon
+    function tryToCatchPokemon(id, pokemonName, type, sprite) {
+        if (pokeballsLeft > 0) {
+          const isSuccess = Math.random() <= catchProbability;
+    
+          if (isSuccess && team.length < teamLimit) {
+            team.push({name: pokemonName, type: type, sprite: sprite });
+            showMessage(`¡Felicidades! Has atrapado a ${pokemonName} y lo has añadido a tu equipo.`);
+          } else if (isSuccess && team.length >= teamLimit) {
+            showMessage(`Tu equipo está lleno. No puedes atrapar más Pokémon.`);
+          } else {
+            showMessage(`¡Oh no! ${pokemonName} se ha escapado.`);
+          }
+    
+          pokeballsLeft--;
+          updatePokeballsLeftText();
+    
+          if (pokeballsLeft === 0) {
+            showMessage(`Te has quedado sin Pokéballs. Visita la tienda más cercana para conseguir más por ${pokeballPrice} creditos cada una.`);
+          }
+    
+          saveTeamAndPokeballs(); // Guardar el equipo y las Pokébolas restantes en el localStorage
+    
+          displayTeamMembers();
+        } else {
+          showMessage(`Te has quedado sin Pokéballs. Visita la tienda más cercana para conseguir más por ${pokeballPrice} creditos cada una.`);
+        }
+    }
 
+    // Funcion para actualziar pokeballs
+    function updatePokeballsLeftText() {
+        pokeballsLeftText.textContent = `Pokéballs restantes: ${pokeballsLeft}`;
+    }
+  
+    // Funcion para obtener pokemon y hacer un array
+    function getPokemonInfo (array, numero) {
+        let nombreParcial = array.filter((elemento) => {
+            return elemento.id === numero 
+        })  
 
+        // En caso que no ingrese nada o algo incorrecto, se desbloquea un Easter Egg
+        if (numero < 1 || numero > 9) {
+            const pokemonName = 'Pikachu'
+            const type = 'Electrico'
+            const sprite = './img/0.png'   
+            return {name: pokemonName, type: type, sprite: sprite}
+        } else {
+            const pokemonName = nombreParcial[0].nombre
+            const type = nombreParcial[0].tipo
+            const sprite = nombreParcial[0].sprite
+            return {name: pokemonName, type: type, sprite: sprite}
+        } 
+    }
+  
+    // Funcion mensaje
+    function showMessage(message) {
+      messageText.textContent = message;
+    }
+  
+    function displayTeamMembers() {
+      teamList.innerHTML = ''; // Limpiar la lista de equipo
+        
+      
+      if (team.length === 0) {
+        return; // Salir si el equipo está vacío
+      }
+  
+      const row = document.createElement('div');
+      row.classList.add('col', 'card-grid');
+  
+      team.forEach(pokemon => {
+        const col = document.createElement('div');
+        col.classList.add('col-2');
+  
+        const card = document.createElement('div');
+        card.classList.add('card', 'h-100');
+  
+        const pokemonSprite = document.createElement('img');
+        pokemonSprite.classList.add('card-img-top');
+        pokemonSprite.src = pokemon.sprite;
+        card.appendChild(pokemonSprite);
+  
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+  
+        const pokemonName = document.createElement('h5');
+        pokemonName.classList.add('card-title');
+        pokemonName.textContent = pokemon.name;
+        cardBody.appendChild(pokemonName);
+  
+        const pokemonType = document.createElement('p');
+        pokemonType.classList.add('card-text');
+        pokemonType.textContent = `Tipo: ${pokemon.type}`;
+        cardBody.appendChild(pokemonType);
+  
+        card.appendChild(cardBody);
+        col.appendChild(card);
+        row.appendChild(col);
+      });
+  
+      teamList.appendChild(row);
+    }
+  
+    function saveTeamAndPokeballs() {
+      localStorage.setItem('team', JSON.stringify(team)); // Guardar el equipo en el localStorage
+      localStorage.setItem('pokeballsLeft', pokeballsLeft.toString()); // Guardar las Pokébolas restantes en el localStorage
+    }
+  
+    updatePokeballsLeftText();
+    displayTeamMembers();
 
-
-
-
+  });
